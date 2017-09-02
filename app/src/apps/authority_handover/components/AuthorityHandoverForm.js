2475f2aa3fa4a9414868083f0cbbb6e2ca3ef0e4
wrufesh singh <wrufesh@gmail.com>
2017-09-02 21:50:39 +0545

    form desigh

--- app/src/apps/authority_handover/components/AuthorityHandoverForm.js
+++ app/src/apps/authority_handover/components/AuthorityHandoverForm.js
@@ -27,49 +27,60 @@ class AuthorityHandoverForm extends React.Component {
     return (
       <div className="card">
         <div className="card-block">
+          <table>
+            <thead>
+            <tr>
+              <th colSpan="4">Authority Handover Form</th>
+            </tr>
+            </thead>
+            <tbody>
+              <tr>
+                
+              </tr>
+            </tbody>
+          </table>
           <form>
-        <div className="row">
-          <div className="col-sm-6 col-md-3">
-            <Field name="type" component={renderReactSelectWrapper}
-                   options={this.authority_handover_type}
-                   label="Handover Type"
-            />
-          </div>
-          <div className="col-sm-6 col-md-3">
-            <Field name="parent" component={renderReactSelectWrapper}
-                   options={getOptions('authority_handover', 'authority_handover', '/api/v1/authority-handover/choices/')}
-                   label="Parent Handover"
-            />
-          </div>
-          <div className="col-sm-6 col-md-3">
-            <Field name="beneficiary" component={renderReactSelectWrapper}
-                   options={getOptions('authority_handover', 'beneficiary', '/api/v1/beneficiary/choices/')}
-                   label="Beneficiary Office"
-            />
-          </div>
-          <div className="col-sm-6 col-md-3">
-            <Field name="fiscal_year" component={renderReactSelectWrapper}
-                   options={getOptions('core', 'fiscal_year', '/api/v1/fiscal-year/choices/')}
-                   label="Fiscal Year"
-            />
-          </div>
-          <div className="col-sm-6 col-md-3">
-            <Field name="budget_head" component={renderReactSelectWrapper}
-                   options={getOptions('core', 'budget_head', '/api/v1/budget-head/choices/')}
-                   label="Budget Head"
-            />
-          </div>
-          <div className="col-sm-6 col-md-3">
-            <Field name="priority_code" type="text" label="Priority" component={renderField}/>
-          </div>
-          <div className="col-sm-6 col-md-3">
-            <Field name="date" type="date" label="Date" component={renderField}/>
-          </div>
-        </div>
-        <div>
-          <FieldArray name="budget_distributions" component={renderBudgetDistributions} />
-        </div>
-      </form>
+              <div className="col-sm-6 col-md-3">
+                <Field name="type" component={renderReactSelectWrapper}
+                       options={this.authority_handover_type}
+                       label="Handover Type"
+                />
+              </div>
+              <div className="col-sm-6 col-md-3">
+                <Field name="parent" component={renderReactSelectWrapper}
+                       options={getOptions('authority_handover', 'authority_handover', '/api/v1/authority-handover/choices/')}
+                       label="Parent Handover"
+                />
+              </div>
+              <div className="col-sm-6 col-md-3">
+                <Field name="beneficiary" component={renderReactSelectWrapper}
+                       options={getOptions('authority_handover', 'beneficiary', '/api/v1/beneficiary/choices/')}
+                       label="Beneficiary Office"
+                />
+              </div>
+              <div className="col-sm-6 col-md-3">
+                <Field name="fiscal_year" component={renderReactSelectWrapper}
+                       options={getOptions('core', 'fiscal_year', '/api/v1/fiscal-year/choices/')}
+                       label="Fiscal Year"
+                />
+              </div>
+              <div className="col-sm-6 col-md-3">
+                <Field name="budget_head" component={renderReactSelectWrapper}
+                       options={getOptions('core', 'budget_head', '/api/v1/budget-head/choices/')}
+                       label="Budget Head"
+                />
+              </div>
+              <div className="col-sm-6 col-md-3">
+                <Field name="priority_code" type="text" label="Priority" component={renderField}/>
+              </div>
+              <div className="col-sm-6 col-md-3">
+                <Field name="date" type="date" label="Date" component={renderField}/>
+              </div>
+            </div>
+            <div>
+              <FieldArray name="budget_distributions" component={renderBudgetDistributions}/>
+            </div>
+          </form>
         </div>
       </div>
     )
